f0942cc342e64827c4ef7e42e9bad1bee6e414d2
harm2o <meike@harm2o.de>
2017-10-15 21:27:54 +0200

    concat added to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
+	concat = require('gulp-concat'),
 	coffee = require('gulp-coffee');
 	
-	coffeeScripts = ['components/coffee/*.coffee']
+var	coffeeScripts = ['components/coffee/*.coffee']
+var	jsSources = [
+	'components/scripts/rclick.js',
+	'components/scripts/pixgrid.js',
+	'components/scripts/tagline.js',
+	'components/scripts/template.js'
+	];
 
 gulp.task('coffee', function(){
-	gulp.src()
+	gulp.src(coffeeScripts)
 	.pipe(coffee({bare: true})
 	.on('error', gutil.log))
 	.pipe(gulp.dest('components/scripts'))
+});
+
+gulp.task('js', function(){
+	gulp.src(jsSources)
+	.pipe(concat('script.js'))
+	.pipe(gulp.dest('builds/developement/js'))
 });
